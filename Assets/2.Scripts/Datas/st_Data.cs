9ed4c67921550f019cbd269dd2b1a85180e754b3
karrk <101902544+karrk@users.noreply.github.com>
2024-09-26 15:11:11 +0900

    [Create] 변수, 데이터 집중클래스

---
+++ Assets/2.Scripts/Datas/st_Data.cs
@@ -0,0 +1,7 @@
+/// <summary>
+/// 한번 설정된 값은 반영구적으로 사용되는 데이터
+/// </summary>
+public class st_Data
+{
+    
+}
---
+++ Assets/2.Scripts/Datas/var_Data.cs
@@ -0,0 +1,11 @@
+/// <summary>
+/// 언제든지 접근하여 변경시킬 수 있는 데이터
+/// </summary>
+public class var_Data
+{
+    public string PlayerName;
+    public E_PlayerGrade PlayerGrade;
+
+    public float BGM_Volume;
+    public float SFX_Volume;
+}
