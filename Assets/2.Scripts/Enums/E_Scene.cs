c1f4753b87d26760eb1bb8cd6ad7409fbcfbd5a4
karrk <101902544+karrk@users.noreply.github.com>
2024-09-26 15:12:10 +0900

    [Create] 각 enum 타입 정의

---
+++ Assets/2.Scripts/Enums/E_Scene.cs
@@ -0,0 +1,9 @@
+public enum E_Scene
+{
+    MainMenu,
+    BattleScene,
+    BounsArea,
+    CharacterSelect,
+    Ending,
+    UnknownArea,
+}
---
+++ Assets/2.Scripts/Enums/E_UI.cs
@@ -0,0 +1,6 @@
+public enum ParameterType
+{
+    Void,
+    Float,
+    String,
+}
